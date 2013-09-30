223c85a76d37118e668ef4eb29f28ddc37adc0a2
Louis Simoneau <simoneau.louis@gmail.com>
2013-09-30 16:47:04 +1000

    better git aliases

--- home/.zsh/git.plugin.zsh
+++ home/.zsh/git.plugin.zsh
@@ -1,6 +1,7 @@
 # Git aliases
 alias gu='git push origin $(current_branch)'
-alias gd='git fetch && git rebase origin/$(current_branch)'
+alias gd='git fetch && git rebase -P origin/$(current_branch)'
+alias gff='git fetch && git merge origin/$(current_branch) --ff-only'
 
 function current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
